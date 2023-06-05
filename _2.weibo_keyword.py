a30448b23518662131e2b075ebc4337f890d31ab
MyJersey <1754543329@qq.com>
2023-06-05 10:26:07 +0800

    json formatting

--- _2.weibo_keyword.py
+++ _2.weibo_keyword.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 url_template='http://m.weibo.cn/api/container/getIndex?type=wb&queryVal={}&containerid=100103type=2%26q%3D{}&page={}'
 
 headers = {           
@@ -11,7 +12,7 @@ def fetch_data(query_val,page_id):
     # print(resp.text)
     card_group=json.loads(resp.text)['data']['cards'][0]
     # print(card_group)
-    print('url:',resp.url,'---条数',len(card_group))
+    print('url:',resp.url,'\n','---条数:',len(card_group))
 
     mblogs=[]  #保存处理过的微博
 
@@ -27,6 +28,7 @@ def fetch_data(query_val,page_id):
         'attitudes_count':mblog['attitudes_count']
     }
     print(blog)
+    time.sleep(0.5)
     mblogs.append(blog)
     return mblogs
 
@@ -45,20 +47,20 @@ def fetch_pages(query_val,page_num):
         try:
             mblogs.extend(fetch_data(query_val,page_id))
         except Exception as e:
-            # print(e)
-            pass
+            print(e)
+            
     
     print("去重前",len(mblogs))
     mblogs=remove_dupli(mblogs)
     print("去重后",len(mblogs))
 
     # 保存至 result.json中
-    with open('{}.json'.format(query_val),'w',encoding='utf-8') as f:
-        json.dump(mblogs,f,ensure_ascii=False)
+    with open('{}.json'.format(query_val),'w',encoding='utf-8', newline='\n') as f:
+        data=json.dump(mblogs,f,ensure_ascii=False,indent=1)
         print("已经保存至{}.json".format(query_val))
 
 
 if __name__=='__main__':
-    fetch_pages('无耻之徒',10)
+    fetch_pages('无耻之徒',50)
 
 
